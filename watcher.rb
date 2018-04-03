2ce06c6302895926b6f7b0373e080029a77f1ec9
garmy <admin@garmy.jp>
2018-04-03 13:31:01 +0900

    change push7 to slack, add new company
    - Push通知をpush7によるブラウザ宛からslackに変更
    - 某社に加えて某社の通知も拾うようにした

--- watcher.rb
+++ watcher.rb
@@ -35,42 +35,86 @@ TweetStream.configure do |config|
   config.oauth_token_secret  = conf.gets.chomp
   config.auth_method         = :oauth
 end
-push7appno  = conf.gets.chomp
-push7apikey = conf.gets.chomp
+slackurl    = conf.gets.chomp
 conf.close
 
-uri = URI.parse("https://api.push7.jp/api/v1/"+push7appno+"/send")
+uri = URI.parse(slackurl)
 
 logging('Start: watcher bot started.')
 
-watch_id = 462569554
+watch_id  = 462569554
+watch_id2 = 599957024
 
 begin
 
-  TweetStream::Client.new.follow(watch_id) do |status| 
+  TweetStream::Client.new.follow(watch_id, watch_id2) do |status|
     if status.user.id == watch_id then
       logging("Getting status from webpage")
-      sleep 10
+      sleep 20
 
       url = 'http://www.odakyu.jp/cgi-bin/user/emg/emergency_bbs.pl'
       html = open(url).read.encode("utf-8", "Shift_JIS")
-      doc = Nokogiri::HTML(html, nil, 'Shift_JIS')
+      doc = Nokogiri::HTML(html, nil, 'utf-8')
       situation = doc.css('div.left_dotline_b')[0].search("dd").text
-
-      logging("Push Notification executing tweet from #{status.user.id}")
+      detail = []
+      doc.css('div.left_dotline_b').each_with_index { | threeline, i |
+        if i > 0 then
+          detail.push({
+            "title" => threeline.search("dt").text,
+            "value" => threeline.search("dd").text
+            }
+          )
+        end
+      }
+      logging("Slack post executing tweet from #{status.user.id}")
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
       request = Net::HTTP::Post.new(uri.request_uri)
       request["Content-Type"] = "application/json"
       payload = {
-        "title" => status.text,
-        "body"  => situation,
-        "icon"  => "https://pbs.twimg.com/profile_images/1982673843/0216odakyu_twitter.jpg",
-        "url"   => "https://twitter.com/#{status.user.screen_name}/status/#{status.id}",
-        "apikey" => push7apikey}.to_json
+        "mrkdwn"  => true,
+        "pretext" => "鉄道運行状況通知（小田急）",
+        "text"    => "#{status.text}\n#{situation}\n<https://twitter.com/#{status.user.screen_name}/status/#{status.id}|link to tweet>",
+        "mrkdwn_in" => [ "text" ],
+        "color"  => "warning",
+        "fields" => detail}.to_json
       request.body = payload
       response = https.request(request)
     end
+
+    if status.user.id == watch_id2 then
+      logging("Getting status from webpage")
+      sleep 10
+
+      url = 'https://www.keio.co.jp/unkou/unkou_pc.html'
+      html = open(url).read.encode("utf-8", "Shift_JIS")
+      doc = Nokogiri::HTML(html, nil, 'utf-8')
+      situation = doc.css('p.status').text
+
+      logging("Slack post executing tweet from #{status.user.id}")
+      if ( Time.now.strftime('%H:%M') == '07:00' ) then
+        slack_color = "good"
+      elsif ( Time.now.strftime('%H:%M') == '17:00' ) then
+        slack_color = "good"
+      else
+        slack_color = "warning"
+      end
+
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request["Content-Type"] = "application/json"
+      payload = {
+        "mrkdwn"  => true,
+        "pretext" => "鉄道運行状況通知（京王）\n#{status.text}\n<https://twitter.com/#{status.user.screen_name}/status/#{status.id}|link to tweet>",
+        "text"    => situation,
+        "mrkdwn_in" => [ "text" ],
+        "color" => slack_color,
+      }.to_json
+      request.body = payload
+      response = https.request(request)
+    end
+
   end
 
 ensure

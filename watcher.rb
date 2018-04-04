652ce0632a51c422c79fef0bff447719c35a5539
garmy <admin@garmy.jp>
2018-04-04 22:18:00 +0900

    update slack format
    slackに投げるメッセージのJSONが一部変だったのを直しつつ、平常運転判断を時刻ではなく文面を用いるようにした

--- watcher.rb
+++ watcher.rb
@@ -50,21 +50,17 @@ begin
   TweetStream::Client.new.follow(watch_id, watch_id2) do |status|
     if status.user.id == watch_id then
       logging("Getting status from webpage")
-      sleep 20
+      sleep 30
 
       url = 'http://www.odakyu.jp/cgi-bin/user/emg/emergency_bbs.pl'
       html = open(url).read.encode("utf-8", "Shift_JIS")
       doc = Nokogiri::HTML(html, nil, 'utf-8')
-      situation = doc.css('div.left_dotline_b')[0].search("dd").text
       detail = []
-      doc.css('div.left_dotline_b').each_with_index { | threeline, i |
-        if i > 0 then
-          detail.push({
-            "title" => threeline.search("dt").text,
-            "value" => threeline.search("dd").text
-            }
-          )
-        end
+      doc.css('div.left_dotline_b').each { | threeline |
+        detail.push({
+          "title" => threeline.search("dt").text,
+          "value" => threeline.search("dd").text
+        })
       }
       logging("Slack post executing tweet from #{status.user.id}")
       https = Net::HTTP.new(uri.host, uri.port)
@@ -72,19 +68,22 @@ begin
       request = Net::HTTP::Post.new(uri.request_uri)
       request["Content-Type"] = "application/json"
       payload = {
-        "mrkdwn"  => true,
-        "pretext" => "鉄道運行状況通知（小田急）",
-        "text"    => "#{status.text}\n#{situation}\n<https://twitter.com/#{status.user.screen_name}/status/#{status.id}|link to tweet>",
-        "mrkdwn_in" => [ "text" ],
-        "color"  => "warning",
-        "fields" => detail}.to_json
+        "attachments" => [
+          {
+            "text" => "\*鉄道運行状況通知（小田急）\*\n#{status.text}\n<https://twitter.com/#{status.user.screen_name}/status/#{status.id}|link to tweet>",
+            "mrkdwn_in" => [ "text" ],
+            "color"  => "warning",
+            "fields" => detail
+          }
+        ]
+      }.to_json
       request.body = payload
       response = https.request(request)
     end
 
     if status.user.id == watch_id2 then
       logging("Getting status from webpage")
-      sleep 10
+      sleep 30
 
       url = 'https://www.keio.co.jp/unkou/unkou_pc.html'
       html = open(url).read.encode("utf-8", "Shift_JIS")
@@ -92,9 +91,7 @@ begin
       situation = doc.css('p.status').text
 
       logging("Slack post executing tweet from #{status.user.id}")
-      if ( Time.now.strftime('%H:%M') == '07:00' ) then
-        slack_color = "good"
-      elsif ( Time.now.strftime('%H:%M') == '17:00' ) then
+      if ( status.text.match('平常') ) then
         slack_color = "good"
       else
         slack_color = "warning"
@@ -105,11 +102,19 @@ begin
       request = Net::HTTP::Post.new(uri.request_uri)
       request["Content-Type"] = "application/json"
       payload = {
-        "mrkdwn"  => true,
-        "pretext" => "鉄道運行状況通知（京王）\n#{status.text}\n<https://twitter.com/#{status.user.screen_name}/status/#{status.id}|link to tweet>",
-        "text"    => situation,
-        "mrkdwn_in" => [ "text" ],
-        "color" => slack_color,
+        "attachments" => [
+          {
+            "text" => "\*鉄道運行状況通知（京王）\*\n#{status.text}\n<https://twitter.com/#{status.user.screen_name}/status/#{status.id}|link to tweet>",
+            "mrkdwn_in" => [ "text" ],
+            "color" => slack_color,
+		        "fields" => [
+              {
+                "title" => "運行状況",
+                "value" => situation
+              }
+            ]
+          }
+        ]
       }.to_json
       request.body = payload
       response = https.request(request)
